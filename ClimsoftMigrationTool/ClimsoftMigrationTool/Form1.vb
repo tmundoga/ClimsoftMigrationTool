a120e79b70eec26a4253636065ee9e47e9ff5bbd
Ian Edwards <ian@myacorn.com>
2015-11-11 22:15:36 +0300

    Changed form layout and refactored initial code

--- ClimsoftMigrationTool/ClimsoftMigrationTool/Form1.vb
+++ ClimsoftMigrationTool/ClimsoftMigrationTool/Form1.vb
@@ -3,23 +3,23 @@
     Public Property dbConnectionV4 As New MySql.Data.MySqlClient.MySqlConnection
 
     Private Sub cmdOK_Click(sender As Object, e As EventArgs) Handles cmdOK.Click
-        dbConnectionV3.ConnectionString = "server=" & txtv3server.Text & ";" & "database=" &
-            txtv3Database.Text & ";" & "port=" & txtv3Port.Text & ";" &
-            "uid=" & txtv3Username.Text & ";" &
-            "pwd=" & txtv3Password.Text & ";"
-        dbConnectionV4.ConnectionString = "server=" & txtv4server.Text & ";" & "database=" &
-            txtv4Database.Text & ";" & "port=" & txtv4Port.Text & ";" & "uid=" &
-            txtv4Username.Text & ";" & "pwd=" & txtv4Password.Text & ";"
+        Dim connectionString As String
         Try
+            ' TODO: Sanitise inputs from text boxes
+            connectionString = "server={0};database={1};port={2};uid={3};pwd={4}"
+
+            dbConnectionV3.ConnectionString = String.Format(connectionString,
+                txtV3Server.Text, cboV3Database.Text, CInt(txtV3Port.Text),
+                txtV3Username.Text, txtV3Password.Text)
+            dbConnectionV4.ConnectionString = String.Format(connectionString,
+                txtV4Server.Text, cboV4Database.Text, CInt(txtV4Port.Text),
+                txtV4Username.Text, txtV4Password.Text)
             dbConnectionV3.Open()
             dbConnectionV4.Open()
-            MsgBox("Successfully connected to both databases")
             copyStationData()
         Catch ex As Exception
             MsgBox(ex.Message)
-
         End Try
-
     End Sub
 
     Private Sub cmdClose_Click(sender As Object, e As EventArgs) Handles cmdClose.Click
@@ -27,35 +27,39 @@
     End Sub
 
     Private Sub copyStationData()
+        Dim command As New MySql.Data.MySqlClient.MySqlCommand
         Dim dataAdapter As New MySql.Data.MySqlClient.MySqlDataAdapter
         Dim ds As New DataSet
         Dim sql As String
         Dim stationId As String
-        Dim totalrows As Integer
 
         Try
-            sql = "Select stationId from station limit 50"
-            'For Id = & "stationId=" & ";"
-            'Next stationId
+            'sql = "SELECT stationId FROM station"
+            sql = "SELECT recorded_from FROM observation GROUP BY recorded_from;"
             dataAdapter = New MySql.Data.MySqlClient.MySqlDataAdapter(sql, dbConnectionV3)
             dataAdapter.Fill(ds, "station")
 
-            totalrows = ds.Tables("station").Rows.Count
+            ' Create connection for UPDATE to v4 database
+            command.Connection = dbConnectionV4
 
-            For count = 0 To totalrows - 1 Step 1
-                stationId = ds.Tables("station").Rows(count).Item("stationId")
+            For Each row In ds.Tables("station").Rows
+                stationId = row.Item("recorded_from")
+                If stationId <> "" Then
+                    sql = "SELECT * FROM observation WHERE recorded_from='" & stationId & "';"
+                    dataAdapter = New MySql.Data.MySqlClient.MySqlDataAdapter(sql, dbConnectionV3)
+                    dataAdapter.Fill(ds, "observationsForCurrentStation")
+                    MsgBox(ds.Tables("observationsForCurrentStation").Rows.Count)
+                    'command.CommandText = "UPDATE ..."
+                    'command.ExecuteNonQuery()
 
-                sql = "Select * from observationfinal where stationId=" & stationId & ";"
-                dataAdapter = New MySql.Data.MySqlClient.MySqlDataAdapter(sql, dbConnectionV3)
-                dataAdapter.Fill(ds, "observationsForStation")
-
-                MsgBox(stationId)
+                    ' Clear current observation values from dataset
+                    ds.Tables("observationsForCurrentStation").Clear()
+                Else
+                    ' TODO: Add any failures to log file
+                End If
             Next
-
         Catch ex As Exception
             MsgBox(ex.Message)
-
         End Try
-
     End Sub
 End Class
